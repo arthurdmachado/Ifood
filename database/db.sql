fe193342ef82f191a8f52ef243ff15e016de4f79
arthurdmachado <arthur_d_machado@estudante.sesisenai.org.br>
2026-08-27 12:19:06 -0300

    add - banco criado

--- database/db.sql
+++ database/db.sql
@@ -9,4 +9,21 @@ CREATE TABLE clientes (
     endereco VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE restaurante
+CREATE TABLE restaurante (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    categoria VARCHAR(200) NOT NULL,
+    telefone VARCHAR(100) NOT NULL,
+    endereco VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE pedido (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    data_pedido DATE TIME NOT NULL,
+    valor DECIMAL(10,2) NOT NULL, 
+    status VARCHAR(200) NOT NULL,
+    clientes_id INT,
+    restaurante_id INT,
+    FOREIGN KEY (clientes_id) REFERENCES clientes(id),
+    FOREIGN KEY (restaurante_id) REFERENCES restaurante(id)
+);
